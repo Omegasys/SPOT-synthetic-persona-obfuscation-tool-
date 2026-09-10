0802bdd6fda185d07b8cb03bb8eafaf04187c89c
Omega <emomega@proton.me>
2026-09-09 23:32:09 -0400

    Create __init__.py

---
+++ src/spot/__init__.py
@@ -0,0 +1,13 @@
+"""SPOT - Synthetic Persona Obfuscation Tool.
+
+SPOT is an open-source privacy research tool for creating,
+isolating, and managing synthetic personas and controlled
+synthetic activity.
+"""
+
+from .version import **version**, VERSION
+
+**all** = [
+"**version**",
+"VERSION",
+]
